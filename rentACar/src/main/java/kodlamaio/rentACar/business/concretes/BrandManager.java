1647207adb37d134eebe0f781eaa27b8f416386a
Murat-Selim <66679051+Murat-Selim@users.noreply.github.com>
2022-11-06 21:25:18 +0300

    Initial commit

---
+++ rentACar/src/main/java/kodlamaio/rentACar/business/concretes/BrandManager.java
@@ -0,0 +1,26 @@
+package kodlamaio.rentACar.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.rentACar.business.abstracts.BrandService;
+import kodlamaio.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlamaio.rentACar.entities.concretes.Brand;
+
+@Service
+public class BrandManager implements BrandService{
+	private BrandRepository brandRepository;
+
+	@Autowired
+    public BrandManager(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+	@Override
+	public List<Brand> getAll() {
+		return brandRepository.getAll();
+	}
+
+}
---
+++ rentACar/src/main/java/kodlamaio/rentACar/webApi/controllers/BrandsController.java
@@ -0,0 +1,28 @@
+package kodlamaio.rentACar.webApi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.rentACar.business.abstracts.BrandService;
+import kodlamaio.rentACar.entities.concretes.Brand;
+
+@RestController
+@RequestMapping("/api/brands")
+public class BrandsController {
+	private BrandService brandService;
+
+	@Autowired
+    public BrandsController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @GetMapping("/getall")
+    public List<Brand> getAll() {
+        return brandService.getAll();
+    }
+
+}
